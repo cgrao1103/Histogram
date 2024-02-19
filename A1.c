1d8765b400126111bba53f7025ee9c0a5af4ad18
cgrao1103 <cgrao@uoguelph.ca>
2024-02-18 20:54:01 -0500

    A1 OS

--- A1.c
+++ A1.c
@@ -8,13 +8,24 @@
 #include <sys/wait.h>
 
 #define MAX_CHAR 26
+#define MAX_CHILDREN 100
 
-void compute_histogram(const char *filename, int pipe_fd) {
+int pipes[MAX_CHILDREN][2]; // Global array of pipes
+pid_t child_pids[MAX_CHILDREN] = {0}; // Initialize with zeros to track child PIDs
+char *child_filenames[MAX_CHILDREN]; // Array to store child filenames
+
+void compute_histogram(const char *filename, int pipe_fd, int child_index) {
+    // Install SIGINT handler for child processes
+    signal(SIGINT, SIG_DFL); 
+
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         perror("fopen");
+        close(pipe_fd);
         exit(EXIT_FAILURE);
     }
+
+    printf("Processing file: %s\n", filename);
 
     int histogram[MAX_CHAR] = {0};
     int ch;
@@ -31,6 +42,89 @@ void compute_histogram(const char *filename, int pipe_fd) {
     }
 
     fclose(file);
+
+    // Sleep for 10+3*i seconds
+    sleep(10 + 3 * child_index);
+
+    // Construct output filename without extension
+    char output_filename[300]; // Adjust buffer size as needed
+    const char *file_basename = strrchr(filename, '/');
+    if (file_basename == NULL)
+        file_basename = filename;
+    else
+        file_basename++;  // Move past the '/'
+    char file_basename_no_ext[256];
+    strncpy(file_basename_no_ext, file_basename, strchr(file_basename, '.') - file_basename);
+    file_basename_no_ext[strchr(file_basename, '.') - file_basename] = '\0';
+    snprintf(output_filename, sizeof(output_filename), "%s%d.hist", file_basename_no_ext, getpid());
+
+    // Write histogram to output file
+    FILE *histogram_file = fopen(output_filename, "w");
+    if (histogram_file == NULL) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+    for (int j = 0; j < MAX_CHAR; j++) {
+        fprintf(histogram_file, "%c %d\n", 'a' + j, histogram[j]);
+    }
+    fclose(histogram_file);
+
+    printf("Histogram written to: %s\n", output_filename);
+
+    // Terminate normally
+    exit(EXIT_SUCCESS);
+}
+
+void sigchld_handler(int signum) {
+    int status;
+    pid_t pid;
+    
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            printf("Child with PID %d terminated successfully.\n", pid);
+            
+            // Read histogram from the corresponding child's pipe
+            int index;
+            for (index = 0; index < MAX_CHILDREN; index++) {
+                if (child_pids[index] == pid) {
+                    break;
+                }
+            }
+            
+            if (index < MAX_CHILDREN) {
+                int histogram[MAX_CHAR];
+                close(pipes[index][1]);
+
+                if (read(pipes[index][0], histogram, MAX_CHAR * sizeof(int)) == -1) {
+                    perror("read");
+                    exit(EXIT_FAILURE);
+                }
+                close(pipes[index][0]);
+
+                char filename[256];
+                snprintf(filename, sizeof(filename), "%s%d.hist", child_filenames[index], pid);  // Use PID of the child process
+                FILE *histogram_file = fopen(filename, "w");
+                if (histogram_file == NULL) {
+                    perror("fopen");
+                    exit(EXIT_FAILURE);
+                }
+                for (int j = 0; j < MAX_CHAR; j++) {
+                    fprintf(histogram_file, "%c %d\n", 'a' + j, histogram[j]);
+                }
+                fclose(histogram_file);
+            }
+        } else {
+            printf("Child with PID %d terminated abnormally.\n", pid);
+        }
+    }
+}
+
+void sigint_handler(int signum) {
+    for (int i = 0; i < MAX_CHILDREN; i++) {
+        if (child_pids[i] != 0) {
+            kill(child_pids[i], SIGINT);
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -39,55 +133,60 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    int num_children = argc - 1;
-    int pipes[num_children][2];
+    int num_children = 0;
 
-    for (int i = 0; i < num_children; i++) {
-        if (pipe(pipes[i]) == -1) {
-            perror("pipe");
-            exit(EXIT_FAILURE);
+    signal(SIGCHLD, sigchld_handler); // Register SIGCHLD handler
+    signal(SIGINT, sigint_handler); // Register SIGINT handler
+
+    for (int i = 1; i < argc; i++) { // Start from index 1 to skip program name
+        if (strcmp(argv[i], "SIG") == 0) {
+            // Send SIGINT to corresponding child process
+            if (num_children >= MAX_CHILDREN) {
+                fprintf(stderr, "Too many files provided.\n");
+                exit(EXIT_FAILURE);
+            }
+            if (child_pids[num_children] != 0) {
+                kill(child_pids[num_children], SIGINT);
+            }
+            num_children++;
+        } else {
+            // Process regular file
+            if (num_children >= MAX_CHILDREN) {
+                fprintf(stderr, "Too many files provided.\n");
+                exit(EXIT_FAILURE);
+            }
+            if (pipe(pipes[num_children]) == -1) {
+                perror("pipe");
+                exit(EXIT_FAILURE);
+            }
+
+            pid_t pid = fork();
+            if (pid == 0) {
+                close(pipes[num_children][0]);
+                compute_histogram(argv[i], pipes[num_children][1], num_children);
+                close(pipes[num_children][1]);
+                exit(EXIT_SUCCESS);
+            } else if (pid < 0) {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            } else {
+                child_pids[num_children] = pid; // Track child PID
+                child_filenames[num_children] = argv[i]; // Track child filename
+            }
+            num_children++;
         }
     }
 
-    for (int i = 0; i < num_children; i++) {
-        pid_t pid = fork();
-        if (pid == 0) {
-            close(pipes[i][0]);
-            compute_histogram(argv[i + 1], pipes[i][1]);
-            close(pipes[i][1]);
-            exit(EXIT_SUCCESS);
-        } else if (pid < 0) {
-            perror("fork");
-            exit(EXIT_FAILURE);
+    // Wait for all children to terminate
+    while (num_children > 0) {
+        int status;
+        pid_t pid = wait(&status);
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            printf("Child with PID %d terminated successfully.\n", pid);
+        } else {
+            printf("Child with PID %d terminated abnormally.\n", pid);
         }
-    }
-
-    for (int i = 0; i < num_children; i++) {
-        int histogram[MAX_CHAR];
-        close(pipes[i][1]);
-
-        if (read(pipes[i][0], histogram, MAX_CHAR * sizeof(int)) == -1) {
-            perror("read");
-            exit(EXIT_FAILURE);
-        }
-        close(pipes[i][0]);
-
-        char filename[20];
-        sprintf(filename, "file%d.hist", i + 1);  // Use index of the child process
-        FILE *histogram_file = fopen(filename, "w");
-        if (histogram_file == NULL) {
-            perror("fopen");
-            exit(EXIT_FAILURE);
-        }
-        for (int j = 0; j < MAX_CHAR; j++) {
-            fprintf(histogram_file, "%c %d\n", 'a' + j, histogram[j]);
-        }
-        fclose(histogram_file);
-    }
-
-    int status;
-    for (int i = 0; i < num_children; i++) {
-        wait(&status);
+        num_children--;
     }
 
     return 0;
